1271ade2409948e86f0111617e592079c8e89e19
joshycool13 <103452581+joshycool13@users.noreply.github.com>
2022-04-19 13:47:09 -0500

    hell

--- Wordle.java
+++ Wordle.java
@@ -1,1 +1,34 @@
-gaming
+import java.util.*;
+
+public class Wordle{
+	public static void main(String args[]) {
+		Scanner kb = new Scanner(System.in);
+		
+		/* get zee random numbar somehow
+		 * 
+		 * put zee number in va variable
+		 */
+		
+		System.out.println("It is time to play a game, a game which we call wordle\n"
+				+ "You will have 6 chances to guess the 5 letter word we are thinking\n"
+				+ "We will tell you which letters are in the word when you guess them\n"
+				+ "We will also tell you if the letters are in the correct place\n"
+				+ "Good luck. We'll see if you make it.");
+		
+		System.out.print("\nPrint your word, plebian:");
+		
+		String word = kb.next();
+		/* do string to character converter
+		 * 
+		 * then die (irl)
+		 * 
+		 * then check the letters to the actual word
+		 * 
+		 * and print out things for correct guess and whatnot
+		 * 
+		 * then die (again)
+		 * 
+		 * then loop this hell that we are in
+		 */
+	}
+}
